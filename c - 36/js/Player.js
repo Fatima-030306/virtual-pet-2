2542f957f206607e022614a994d27ba38ff5a0b7
Fatima-030306 <84084827+Fatima-030306@users.noreply.github.com>
2021-08-16 18:49:04 +0530

    Add files via upload

---
+++ c - 36/js/Player.js
@@ -0,0 +1,21 @@
+class   Player{
+    construtor(){
+        this.index=null
+        this.distance=0
+        this.name=null
+    }
+    getCount(){
+        var playerCountRef  = database.ref('playerCount');
+        playerCountRef.on("value",(data)=>{
+           playerCount = data.val();
+        })
+       
+      }
+    
+      updateCount(count){
+        database.ref('/').update({
+          playerCount: count
+        });
+      }
+    
+    }
